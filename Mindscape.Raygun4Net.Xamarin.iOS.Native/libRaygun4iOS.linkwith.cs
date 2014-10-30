ff8a9af4ba26ddf642cdd01dab704818c57f814f
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-10-30 15:20:42 -0500

    Set linker options to see if we can keep app size down

--- Mindscape.Raygun4Net.Xamarin.iOS.Native/libRaygun4iOS.linkwith.cs
+++ Mindscape.Raygun4Net.Xamarin.iOS.Native/libRaygun4iOS.linkwith.cs
@@ -2,4 +2,5 @@ using System;
 using ObjCRuntime;
 using Foundation;
 
-[assembly: LinkWith ("libRaygun4iOS.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkerSafe]
+[assembly: LinkWith ("libRaygun4iOS.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, SmartLink = true)]
