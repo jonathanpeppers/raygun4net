7939299d0f00a7bc611c9f874dceb6152e26cf63
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-10-30 15:20:24 -0500

    Ported native library to unified Xamarin APIs

--- Mindscape.Raygun4Net.Xamarin.iOS.Native/libRaygun4iOS.linkwith.cs
+++ Mindscape.Raygun4Net.Xamarin.iOS.Native/libRaygun4iOS.linkwith.cs
@@ -1,4 +1,5 @@
 using System;
-using MonoTouch.ObjCRuntime;
+using ObjCRuntime;
+using Foundation;
 
 [assembly: LinkWith ("libRaygun4iOS.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
